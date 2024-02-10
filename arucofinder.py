0f59c3537509797a80669d00dfe75f462954dae3
Cayden Covarrubias <cjcovarr@calpoly.edu>
2024-02-10 13:53:03 -0800

    added finding the closest id to center [needs testing]

--- arucofinder.py
+++ arucofinder.py
@@ -56,6 +56,9 @@ def main():
         # Draw the detected markers and display the frame
         frame = aruco.drawDetectedMarkers(frame, corners, ids)
         cv2.imshow("Frame", frame)
+        
+        # create dictionary matching ids to distances from center
+        distances = {}
 
         # Print the detected frame coordinates
         # detect which way aruco marker needs to move to be centered
@@ -70,28 +73,40 @@ def main():
             write_read('0')
             marker_found = False
 
-        # found = 0
-        # if ids is not None:
+        found = 0
+        if ids is not None:
+            found = 1
+            for i in range(len(ids)):
+                print(corners[i][0])
+                if corners[i][0][0][0] < 200:
+                    direction = LEFT
+                elif corners[i][0][0][0] > 400:
+                    direction = RIGHT
+                elif corners[i][0][0][1] < 200:
+                    direction = UP
+                elif corners[i][0][0][1] > 400:
+                    direction = DOWN
+                else:
+                    direction = CENTERED
+                    
+        if ids is not None:
+            for i in range(len(ids)):
+                distances[ids[i]] = distance_from_cen(corners[i][0][0])
+        
+        # print the closest id to the center
+        if len(distances) > 0:
+            closest_id = min(distances, key=distances.get)
+            print("Closest ID: ", closest_id)
+            # print("Direction: ", direction)
+        else:
+            print("No Aruco Markers Found")
+            # print("Direction: ", direction)
 
-        #     found = 1
-        #     for i in range(len(ids)):
-        #         print(corners[i][0])
-        #         if corners[i][0][0][0] < 200:
-        #             direction = LEFT
-        #         elif corners[i][0][0][0] > 400:
-        #             direction = RIGHT
-        #         elif corners[i][0][0][1] < 200:
-        #             direction = UP
-        #         elif corners[i][0][0][1] > 400:
-        #             direction = DOWN
-        #         else:
-        #             direction = CENTERED
-
-        # print(direction)
-        # if(direction == CENTERED):
-        #     write_read('1')
-        # else:
-        #     write_read('0')
+        print(direction)
+        if(direction == CENTERED):
+            write_read('1')
+        else:
+            write_read('0')
         
 
 
@@ -106,60 +121,11 @@ def write_read(x):
 	data = arduino.readline() 
 	return data 
 
+def distance_from_cen(coords):
+    return (coords[0] - 320)**2 + (coords[1] - 240)**2
+
 
 #run main
 if __name__ == "__main__":
     main()
 
-
-# """
-# finds the aruco markers in a video feed
-# """
-
-# import cv2
-# import cv2.aruco as aruco
-
-# import serial #ard
-# import time #ard
-
-# arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1) #ard
-
-# def write_read(x):
-#     arduino.write(bytes(x, 'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return data
-
-
-# FRIENDLY = 23
-# BAD = 24
-
-# # Define the ArUco dictionary and parameters
-# aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
-
-# # Define the detector parameters
-# parameters = aruco.DetectorParameters()
-
-# detector = aruco.ArucoDetector(aruco_dict, parameters)
-
-# # Define the video capture device
-# cap = cv2.VideoCapture(0)
-
-# # Continuously capture and process frames
-# while True:
-#     # Capture a frame
-#     ret, frame = cap.read()
-
-#     # Detect the markers
-#     corners, ids, rejected = detector.detectMarkers(frame)
-
-#     # Draw the detected markers and display the frame
-#     frame = aruco.drawDetectedMarkers(frame, corners, ids)
-#     cv2.imshow("Frame", frame)
-
-#     num = input("Enter a number: ")
-#     value = write_read(num)
-
-#     # Exit if the user presses the "q" key
-#     if cv2.waitKey(1) & 0xFF == ord("q"):
-#         break
